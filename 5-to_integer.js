2f91fda16b911a4201a12ab7081a94b8e5f94b2f
uwakwe nneoma <uwakwenneoma@yahoo.com>
2025-11-09 18:45:41 +0100

    Fix 5-to_integer.js to pass all checker cases

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,7 +1,7 @@
 ﻿const arg = process.argv[2];
 const number = parseInt(arg, 10);
 
-if (!isNaN(number)) {
+if (arg && !isNaN(number)) {
     console.log(My number: );
 } else {
     console.log("Not a number");

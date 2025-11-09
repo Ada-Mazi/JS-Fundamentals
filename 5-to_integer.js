c5acf0757c107d7d1344a3c1e95b845c4114da12
uwakwe nneoma <uwakwenneoma@yahoo.com>
2025-11-09 19:49:36 +0100

    fix: Correct integer conversion script

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,12 +1,9 @@
-﻿const arg = process.argv[2];
+﻿#!/usr/bin/node
 
-if (!arg) {
-  console.log('Not a number');
+const arg = process.argv[2];
+
+if (arg === undefined || isNaN(Number(arg))) {
+    console.log("Not a number");
 } else {
-  const number = parseInt(arg, 10);
-  if (isNaN(number)) {
-    console.log('Not a number');
-  } else {
-    console.log(\My number: \\);
-  }
+    console.log("My number: " + parseInt(arg));
 }

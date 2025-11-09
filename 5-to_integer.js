a4054b849d1b743dbaf99a201e9dc0f60a597767
uwakwe nneoma <uwakwenneoma@yahoo.com>
2025-11-09 18:54:53 +0100

    Fix 5-to_integer.js for all checker cases

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,7 +1,7 @@
 ﻿const arg = process.argv[2];
 const number = parseInt(arg, 10);
 
-if (!isNaN(number)) {
+if (!isNaN(number) && arg !== undefined) {
     console.log(My number: );
 } else {
     console.log("Not a number");

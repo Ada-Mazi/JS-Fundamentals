e5a8a2b28b826f5721d40c5805c59cafc3a449d2
uwakwe nneoma <uwakwenneoma@yahoo.com>
2025-11-09 19:04:40 +0100

    Fix 5-to_integer.js syntax for checker

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,12 +1,12 @@
 ﻿const arg = process.argv[2];
 
 if (!arg) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else {
-    const number = parseInt(arg, 10);
-    if (isNaN(number)) {
-        console.log('Not a number');
-    } else {
-        console.log(My number: );
-    }
+  const number = parseInt(arg, 10);
+  if (isNaN(number)) {
+    console.log('Not a number');
+  } else {
+    console.log(My number: );
+  }
 }
